e96dd68958c18e77aacadff066ab91eb1a579fc0
dantelian <985890193@qq.com>
2019-09-25 11:29:46 +0800

    兼容activiti

--- src/main/java/com/example/springbootactiviti/SpringBootActivitiApplication.java
+++ src/main/java/com/example/springbootactiviti/SpringBootActivitiApplication.java
@@ -4,7 +4,7 @@ import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)// 兼容activiti
 public class SpringBootActivitiApplication {
 
 	public static void main(String[] args) {
